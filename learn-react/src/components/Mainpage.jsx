7e1554ed1ee2c19f53511109549efbb797492b5c
yufisu <yusufkeremmalkoc@gmail.com>
2023-11-13 00:37:26 +0300

    added navbar

---
+++ learn-react/src/components/Mainpage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Mainpage = () => {
+  return (
+    <div className="main-page">
+      <div className="welcome-text">
+        <h1 className=""></h1>
+      </div>
+      <div className="banner"></div>
+    </div>
+  )
+}
+export default Mainpage
---
+++ learn-react/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Navbar from './components/Navbar.jsx'
+import Mainpage from './components/Mainpage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App/>
+    <Navbar/>
+    <Mainpage/>
+  </React.StrictMode>,
+)
